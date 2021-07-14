e20ca6e4ecadc106fe5277ad8b7c57ab22b6b53f
saurabh chuadhary <supersaurabh017@gmail.com>
2021-07-14 21:30:59 +0530

    THA16
    https://chessboard007.netlify.app

---
+++ day-16(chess)/chess/src/chess.jsx
@@ -0,0 +1,81 @@
+import "./style.css"
+
+function Chess () {
+     return(
+        <div className="geeks">
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            {/* 1th row */}
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            {/* 2th row */}
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            {/* 3th row */}
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            {/* 4th row */}
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            {/* 5th row */}
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            {/* 6th row */}
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            <div className="ebox"></div>
+            {/* 7th row */}
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+            <div className="obox"></div>
+        </div>
+     )
+}
+
+export default Chess;
---
+++ day-16(chess)/chess/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Chess from "./chess";
+
+ReactDOM.render(<Chess/>,document.getElementById('root'))
+
